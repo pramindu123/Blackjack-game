55dc5b7815b7a837bffc0929f86787732f236226
Pramindu vishwa kumaranayaka <105766695+pramindu123@users.noreply.github.com>
2024-09-16 17:53:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,86 @@
+let player = {
+    name: "Per",
+    chips: 200
+}
+
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": $" + player.chips
+
+function getRandomCard() {
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
+
+function startGame() {
+    if (player.chips==0){
+        messageEl.textContent="200 chips added"
+        player.chips=200
+        playerEl.textContent = player.name + ": $" + player.chips
+
+    }
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        player.chips+=50
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+        player.chips-=50
+    }
+
+    playerEl.textContent = player.name + ": $" + player.chips
+    messageEl.textContent = message
+}
+
+
+function newCard() {
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()        
+    }
+}
+
+function resetCard(){
+        sum = 0
+        cards = []
+        isAlive = false
+        hasBlackJack = false
+        message = "New round"
+        messageEl.textContent = message
+        renderGame()
+}
+
